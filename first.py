5308c110e115c8196bcbc2a965644f337de31ee6
Maxim Dorko <maxim.dorko@gmail.com>
2018-01-17 22:41:19 +0100

    lesson 1

--- first.py
+++ first.py
@@ -1,3 +1,16 @@
 num1 = 10
 num2 = 20
-print (num1 ** num2)
+print (num1, "//", num2, "=", num1//num2)
+num1+=num2
+print (num1)
+print (num1 == num2)
+# Single line comment
+'''
+Multi line comment
+one more line
+'''
+x=True
+y=False
+print(x and y)
+print(x or y)
+print(not y)
