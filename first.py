59eed296a2c5017ac1255704abf56a8c20a95e12
Maxim Dorko <maxim.dorko@gmail.com>
2018-01-17 23:29:06 +0100

    and again

--- first.py
+++ first.py
@@ -1,3 +1,4 @@
+'''
 num1 = 10
 num2 = 20
 print (num1, "//", num2, "=", num1//num2)
@@ -5,16 +6,23 @@ num1+=num2
 print (num1)
 print (num1 == num2)
 # Single line comment
-'''
+
 Multi line comment
 one more line
-'''
 x=True
 y=False
 print(x and y)
 print(x or y)
 print(not y)
 
-a=3
-b=2
-print(a^b)
+a = 3
+b = 2
+b<<=1
+print(a,b)
+print(a is not 3)
+'''
+a = "Maxim is an idiot"
+print("Niki" in a)
+
+servicesTeam = ["Maxim","Szabi","Zsolt"]
+print("Andris" in servicesTeam)
