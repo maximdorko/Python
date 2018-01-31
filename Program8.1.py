860449261d3bc2999b293873f5b37502bda9a8f8
Maxim Dorko <maxim.dorko@gmail.com>
2018-01-31 01:22:20 +0100

    OOP practice

---
+++ Program8.1.py
@@ -0,0 +1,146 @@
+# OOP tutorial
+# Corey Schafer
+
+# Attributes and Methods
+# Employees data
+# Attributes: First name, Last name, 
+
+# Class: A blueprint for an instance of the class
+# Instance of a class: Induvidual instance of the class 
+# Instance variables contains data which unique for each instance
+# Class variables are the same for all instances
+# Class methods are defined by the decarator @classmethod
+# Class methods takes class as the first argument, by convention its "cls"
+# Class methods can be used as alternative constructors
+# Static methods don't pass the instance or the class as argument
+# Static methods behave as normal functions, just we include them in the class
+# if they have some connection with the class
+# If the method doesn't use instance or class, then it should be @staticmethod
+# Subclass inherits the parent class attributes and Methods
+# attributes and methods are looked down to up, from subclass to parentclass
+# subclass is defined by class (parentclass)
+
+class Employee:
+    num_of_employees = 0
+    raise_amount = 1.18
+    def __init__(self, first, last, pay):
+        self.first = first # instance variable
+        self.last = last
+        self.pay = pay
+        self.email = first + "." + last + "@company.com"
+        Employee.num_of_employees += 1 #Runs each time when an instance is created
+    def fullname(self):
+        return "{} {}".format(self.first, self.last)
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+    # @classmethod
+    # def set_arise_amount(cls, amount):
+    #     cls.raise_amount = amount
+
+    # @classmethod
+    # def from_string(cls, employee_string):
+    #     first, last, pay = employee_string.split("-")
+    #     return cls(first, last, pay)
+    
+    # @staticmethod
+    # def is_workday(day):
+    #     if day.weekday() in range(6, 7):
+    #         return False
+    #     return True
+
+class Developer(Employee): # Sub class, inhertited from Employee class.
+    raise_amount = 1.2
+    def __init__(self, first, last, pay, prog_lang):
+        # super().__init__(first, last, pay) # Let the parent class handle these arguments
+        Employee.__init__(self, first, last, pay) # This is used when multiple inheritance exist
+        self.prog_lang = prog_lang
+
+class Manager(Employee):
+    def __init__(self, first, last, pay, list_of_reporting=None):
+        Employee.__init__(self, first, last, pay)
+        if list_of_reporting is None:
+            self.list_of_reporting = []
+        else:
+            self.list_of_reporting = list_of_reporting
+    def add_reporting(self, emp):
+        if emp not in self.list_of_reporting:
+            self.list_of_reporting.append(emp)        
+    def rem_reporting(self, emp):
+        if emp in self.list_of_reporting:
+            self.list_of_reporting.remove(emp)
+    def print_employees(self):
+        for emp in self.list_of_reporting:
+            print("-->", emp.fullname())
+
+
+dev_1 = Developer("John", "Doe", 30000, "C#")
+dev_2 = Developer("James", "Day", 40000, "Python")
+
+mgr_1 = Manager("Judit", "Manman", 90000, [dev_1])
+mgr_1.add_reporting(dev_2)
+mgr_1.print_employees()
+
+print(isinstance(mgr_1, Manager)) # Checks if an instance is part of a Class
+print(issubclass(Developer, Employee)) # Chaks if a subclass memebr of a parent class
+
+#print(dev_1.fullname(), dev_1.prog_lang)
+# print(help(Developer)) # Provides information about the class
+
+# print(dev_1.pay)
+# dev_1.apply_raise()
+# print(dev_1.pay)
+
+# import datetime
+
+# emp1 = Employee("Bruno", "Mars", 50000)
+# emp2 = Employee("Debie", "Gibson", 70000)
+
+# my_date = datetime.date(2018, 1, 29)
+# print(Employee.is_workday(my_date))
+
+# emp_str_1 = "John-Doe-70000"
+# emp_str_2 = "Steve-Smith-60000"
+# emp_str_3 = "Jane-Doe-40000"
+
+# # first, last, pay = emp_str_1.split("-")
+# new_emp1 = Employee.from_string(emp_str_1) # Calling the alternative constructor to create an instance
+# # new_emp1 = Employee(first, last, pay)
+
+# print(new_emp1.fullname())
+# print(new_emp1.email)
+
+
+# Employee.set_arise_amount(1.12)
+
+# print(emp1.raise_amount)
+# print(emp2.raise_amount)
+# print(Employee.raise_amount)
+
+
+# print(Employee.num_of_employees) #Shows 2, since 2 instances were created
+
+# print("{} {}".format(emp1.fullname(), emp1.pay))
+# emp1.apply_raise()
+# print("{} {}".format(emp1.fullname(), emp1.pay))
+# print(emp1.__dict__) # prints the name space of the instance
+
+
+# print(emp1)
+# print(emp2)
+
+# emp1.first = "Bruno"
+# emp2.last = "Mars"
+# emp1.email = "bruno.mars@company.com"
+# emp1.pay = 50000
+
+# emp2.first = "Debbie"
+# emp2.last = "Gibson"
+# emp2.email = "debbie.gibson@company.com"
+# emp2.pay = 70000
+
+# print(emp1.email)
+# print(emp2.email)
+# print("{} {}".format(emp1.first, emp1.last))
+# print("{} {}".format(emp2.first, emp2.last))
+# print(Employee.fullname(emp1)) # Calling the method from the Class, passing instance
+# print(emp1.fullname()) # Calling the method by the instance
---
+++ Program8.2.py
@@ -0,0 +1,61 @@
+# Battle of Warriors
+# A class for warriors with the attributes: Name, Health, Attack, Block
+# Method for attack points
+# Method for block points
+# Class metod for Battle
+import random
+class Warriors:
+    result = 0
+    def __init__(self, name, health, attack, block):
+        self.name = name
+        self.health = health
+        self.attack = attack
+        self.block = block
+    def attack_points(self):
+        return self.attack * random.random()
+    def block_points(self):
+        return self.block * random.random()
+    def print_health(self):
+        print(self.health)
+    @classmethod
+    def battle(cls, warrior1, warrior2):
+        cls.result = int(warrior1.attack_points() - warrior2.block_points())
+        if cls.result <= 0:
+            print("{} attacks {} and misses".format(warrior1.name, warrior2.name))
+        else:
+            print("{} attacks {} and deals {} damage".format(warrior1.name, warrior2.name, cls.result))
+            warrior2.health -= cls.result
+        print("{}'s health {}".format(warrior2.name, warrior2.health))
+        return
+
+warrior_1 = Warriors(input("Warrior name: "), 
+            int(input("Health: ")), 
+            int(input("Attack: ")),
+            int(input("Block: ")))
+warrior_2 = Warriors(input("Warrior name: "), 
+            int(input("Health: ")), 
+            int(input("Attack: ")),
+            int(input("Block: ")))
+attacking_warrior = 1
+print("The match starts")
+while True:
+    if attacking_warrior == 1:
+        Warriors.battle(warrior_1, warrior_2)
+        if warrior_2.health <= 0:
+            print("{} died, {} is the winner".format(warrior_2.name, warrior_1.name))
+            break
+    if attacking_warrior == 2:
+        Warriors.battle(warrior_2, warrior_1)
+        if warrior_1.health <= 0:
+            print("{} died, {} is the winner".format(warrior_1.name, warrior_2.name))
+            break
+    if attacking_warrior == 1:
+        attacking_warrior = 2
+    else:
+        attacking_warrior = 1
+
+        
+
+
+
+
